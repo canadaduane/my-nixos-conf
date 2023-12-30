524065335b4cb9a0ae809716655f7c04a292c1c7
Duane Johnson <duane.johnson@gmail.com>
2023-12-30 16:08:22 -0700

    Add burn-my-windows config

---
+++ system/nixos/gnome/settings/burn-my-windows.nix
@@ -0,0 +1,24 @@
+let
+  burnMyWindowsProfile = pkgs.writeText "nix-profile.conf" ''
+    [burn-my-windows-profile]
+
+    profile-high-priority=true
+    profile-window-type=0
+    profile-animation-type=0
+    fire-enable-effect=false
+    glide-enable-effect=true
+    glide-animation-time=250
+    glide-squish=0.0
+    glide-tilt=0.0
+    glide-shift=0.0
+    glide-scale=0.85
+  '';
+in
+
+{
+  dconf.settings = {
+    "org/gnome/shell/extensions/burn-my-windows" = {
+      active-profile = "${burnMyWindowsProfile}";
+    };
+  };
+}
