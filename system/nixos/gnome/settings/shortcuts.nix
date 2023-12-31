543a5e9f75b48d3cb2e676f258f5fe6ca9886a25
Duane Johnson <duane.johnson@gmail.com>
2023-12-30 22:18:12 -0700

    Fix keybindings

--- system/nixos/gnome/settings/shortcuts.nix
+++ system/nixos/gnome/settings/shortcuts.nix
@@ -35,5 +35,26 @@ in
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = customKeybindings;
     };
+
+    "org/gnome/shell/keybindings" = {
+      focus-active-notification = [ ];
+      open-application-menu = [ ];
+      screenshot = [ ];
+      screenshot-window = [ ];
+      show-screen-recording-ui = [ "<Shift>Print" ];
+      show-screenshot-ui = [ "Print" ];
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+      switch-to-application-5 = [ ];
+      switch-to-application-6 = [ ];
+      switch-to-application-7 = [ ];
+      switch-to-application-8 = [ ];
+      switch-to-application-9 = [ ];
+      toggle-application-view = [ ];
+      toggle-message-tray = [ ];
+      toggle-overview = [ ];
+    };
   };
 }
