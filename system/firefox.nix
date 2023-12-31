dfd97a10915102897dd13bdfe38fddbeca7440ab
Duane Johnson <duane.johnson@gmail.com>
2023-12-30 17:52:28 -0700

    Add bitwarden, privacy badger ff extensions

--- system/firefox.nix
+++ system/firefox.nix
@@ -52,6 +52,14 @@ in
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
         };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
 
