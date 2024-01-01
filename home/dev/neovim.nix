53ff6273d27f695a5760e1dc05d0c66a9a01cc68
Duane Johnson <duane.johnson@gmail.com>
2024-01-01 13:46:18 -0700

    Remove neovim .desktop file

--- home/dev/neovim.nix
+++ home/dev/neovim.nix
@@ -21,5 +21,24 @@ in
       viAlias = true;
       vimAlias = true;
     };
+
+    home.packages = [
+      (hiPrio (pkgs.runCommand "nvim.desktop-hide" { } ''
+        mkdir -p "$out/share/applications"
+        cat "${config.programs.neovim.finalPackage}/share/applications/nvim.desktop" > "$out/share/applications/nvim.desktop"
+        echo "Hidden=1" >> "$out/share/applications/nvim.desktop"
+      ''))
+    ];
   };
 }
+
+# { config, lib, pkgs, ... }:
+# {
+#   environment.systemPackages = [
+#     (lib.hiPrio (pkgs.runCommand "nvim.desktop-hide" { } ''
+#       mkdir -p "$out/share/applications"
+#       cat "${config.programs.neovim.finalPackage}/share/applications/nvim.desktop" > "$out/share/applications/nvim.desktop"
+#       echo "Hidden=1" >> "$out/share/applications/nvim.desktop"
+#     ''))
+#   ];
+# }
