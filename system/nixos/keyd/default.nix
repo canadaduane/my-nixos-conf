9ad31312f32500353b31ab4c5938ae7791a2d20f
Duane Johnson <duane.johnson@gmail.com>
2023-12-31 22:36:11 -0700

    Add keyd keyboard to 'internal'
    Allows Disable-While-Typing to work

--- system/nixos/keyd/default.nix
+++ system/nixos/keyd/default.nix
@@ -6,4 +6,11 @@
   services.keyd.enable = true;
 
   environment.etc."keyd/default.conf".source = ./keyd.conf;
+
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [keyd]
+    MatchUdevType=keyboard
+    MatchVendor=0xFAC
+    AttrKeyboardIntegration=internal
+  '';
 }
